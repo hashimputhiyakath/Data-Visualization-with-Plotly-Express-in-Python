46c1d03af887cb4be698654c06ed53013ade08f8
Hashim Puthiyakath <hashputhiyakath@gmail.com>
2022-09-08 13:46:13 +0530

    Added Bar Charts

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("loading")
+
+
+
+let asideToggler = document.querySelector('.aside-toggler')
+
+let aside = document.querySelector('aside')
+
+console.log(aside)
+
+asideToggler.addEventListener('click', function(){
+    aside.classList.toggle('active')
+    asideToggler.classList.toggle('active')
+})
+
